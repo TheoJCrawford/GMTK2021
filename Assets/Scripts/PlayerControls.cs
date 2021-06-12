999d860c73074ea8fad3cc1dfec7b9bd423b29b7
Theo <theojcrawford@gmail.com>
2021-06-12 11:18:19 +1000

    Added actual movement
    I hope

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,44 @@ public class PlayerControls : MonoBehaviour
 {
     private PlayerInput _input;
     private PlayerMotor _motor;
-    // Start is called before the first frame update
+
+    private InputAction _moveAction;
+    private InputAction _JumpAction;
+
+    public int jumpLimit = 1;
+
+    private int _remainingJump;
     void Start()
     {
+        _motor = GetComponent<PlayerMotor>();
         _input = GetComponent<PlayerInput>();
+        _moveAction = _input.actions.FindAction("Horizontal Movement");
+        _JumpAction = _input.actions.FindAction("Jump");
+        ResetJumping();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _moveAction.performed += TakeMoveInput;
+        _moveAction.canceled += TakeMoveInput;
+        _JumpAction.performed += TakeJumpAction;
+        _JumpAction.canceled += TakeJumpAction;
+    }
+    private void TakeMoveInput(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.ReadValue<float>() == 0)
+        {
+            
+        }
+    }
+    private void TakeJumpAction(InputAction.CallbackContext obj)
+    {
         
+    }
+
+    private void ResetJumping()
+    {
+        _remainingJump = jumpLimit;
     }
 }
