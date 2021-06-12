7f64106ced190af1ad131151f0c1876d64c30a05
Theo <theojcrawford@gmail.com>
2021-06-12 10:07:11 +1000

    The big dump. Go nuts

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput), typeof(PlayerMotor))]
 public class PlayerControls : MonoBehaviour
 {
+    
+    private PlayerInput _input;
+    private PlayerMotor _motor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _input = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMotor:MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private Vector2 _moveVec;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    public void GetNormalMovement(int move = 0)
+    {
+        _moveVec = new Vector2(move, 0);
+    }
+}
