8d6b2635a17700828e77a7a1c566299a06c36b5f
Theo <theojcrawford@gmail.com>
2021-06-13 14:07:57 +1000

    Oops
    Don't want to spawn on 0 hp

--- Assets/Scripts/Player/CharacterStats.cs
+++ Assets/Scripts/Player/CharacterStats.cs
@@ -5,6 +5,11 @@ public class CharacterStats:MonoBehaviour
     public int maxHealth { get; internal set; } = 100;
     public int curHealth { get; internal set; }
 
+    private void Awake()
+    {
+        ResetHealth();
+    }
+
     public void SetMaxHealth(int hp)
     {
         maxHealth = hp;
